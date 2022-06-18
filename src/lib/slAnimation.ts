23e9654a7ca0e4f3f570d432b974531acd9d5e83
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-19 00:57:09 +0900

    add file

---
+++ src/lib/slAnimation.ts
@@ -0,0 +1,14 @@
+import { SlAciiArt } from "../slAa.ts";
+export const SlAnimation = () => {
+  SlAciiArt.wheel.map((value) => {
+
+    SlAciiArt.slTop.map((value) => {
+      console.log(value);
+    });
+    value.map((value1) => {
+      console.log(value1);
+    });
+    console.log("\n \n");
+  });
+};
+SlAnimation();
