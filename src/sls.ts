2ceea99046c266f991ee5f62034da068fe99157a
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-24 10:32:55 +0900

    file list をconsole.logするのを削除

--- src/sls.ts
+++ src/sls.ts
@@ -3,7 +3,7 @@ import { ReadDirFile } from "./lib/ReadDir.ts";
 import { ChooesFileName } from "./lib/chooseFileName.ts";
 async function sls() {
   const fileList = await ReadDirFile();
-  console.log(fileList);
+  // console.log(fileList);
   const Sl = SlAnimation({ files: ChooesFileName(fileList) });
   Sl.map((value) => console.log(value));
 }
