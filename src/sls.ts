7c2f7e0a075e887f2a58da14cac5607a50076672
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-24 10:36:05 +0900

    file list を削除した

--- src/sls.ts
+++ src/sls.ts
@@ -3,7 +3,6 @@ import { ReadDirFile } from "./lib/ReadDir.ts";
 import { ChooesFileName } from "./lib/chooseFileName.ts";
 async function sls() {
   const fileList = await ReadDirFile();
-  // console.log(fileList);
   const Sl = SlAnimation({ files: ChooesFileName(fileList) });
   Sl.map((value) => console.log(value));
 }
