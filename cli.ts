e06f01f9c0181c40fd26912a5240c2948ff79c42
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-25 17:32:10 +0900

    引数がなくても実行できるようにした

--- cli.ts
+++ cli.ts
@@ -2,7 +2,7 @@ import { sls } from "./src/sls.ts";
 import { Command } from "./deps.ts";
 const { options, args } = await new Command()
   .name("sls")
-  .arguments("<dir> [output:string]")
+  .arguments("[dir]")
   .description("show filelist with sl")
   .version("1.0.0")
   .parse(Deno.args);
