2e6de19542541644d9557d1c710c0f72fcd90298
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-25 11:56:41 +0900

    wagon をレンダリングする関数を作った

--- src/lib/createWagon.ts
+++ src/lib/createWagon.ts
@@ -1,10 +1,33 @@
 import { SplitArry } from "./spilitArry.ts";
+import { SlAciiArt } from "./slAa.ts";
 type CreateWagonPropsType = {
   files: string[];
 };
 export const CreateWagon = (props: CreateWagonPropsType): string[] => {
-  const FileArrySplit = SplitArry({ arry: props.files, dividedOfNumber: 5 });
-  const ReturnValue: string[] = [];
+  const cargo = SlAciiArt.cargo;
+  const FileArrySplit = SplitArry({
+    arry: props.files,
+    dividedOfNumber: cargo.maxContentHeigt,
+  });
+  const ReturnValue: string[] = [
+    cargo.wogon[0],
+    cargo.wogon[1],
+    cargo.LeftAndRigntWall,
+    cargo.LeftAndRigntWall,
+    cargo.LeftAndRigntWall,
+    cargo.LeftAndRigntWall,
+    cargo.LeftAndRigntWall,
+    cargo.wogon[8],
+    cargo.wogon[9],
+  ];
 
+  FileArrySplit.map((fileList, fileListKey) => {
+    const stringLength = Math.max(
+      ...fileList.map((e) => e.length),
+    );
+
+    fileList.map((file, fileKey) => {
+    });
+  });
   return ReturnValue;
 };
