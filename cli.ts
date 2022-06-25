8ed0687bed1569afca630a94c6d0d73c54c4ce34
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-25 15:47:51 +0900

    表示するディレクトリを選べるようにした

--- cli.ts
+++ cli.ts
@@ -1,8 +1,11 @@
 import { sls } from "./src/sls.ts";
 import { Command } from "./deps.ts";
-const { options } = await new Command()
+const { options, args } = await new Command()
   .name("sls")
+  .arguments("<dir> [output:string]")
   .description("show filelist with sl")
   .version("1.0.0")
   .parse(Deno.args);
-sls();
+// console.log("options ", options, "args", args);
+const Dir: string = args[0] || ".";
+sls(Dir);
