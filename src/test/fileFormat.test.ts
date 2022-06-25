b0e1177d39ea75fc1f8075a50a149b71fdbe65ad
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-25 12:03:41 +0900

    add test

--- src/test/fileFormat.test.ts
+++ src/test/fileFormat.test.ts
@@ -1,7 +1,7 @@
 import { FileFormat } from "../lib/fileFormat.ts";
 import { assertEquals } from "../../deps.ts";
 
-Deno.test("format file names ", () => {
+Deno.test("format file names 1", () => {
   const files = [
     "hello1",
     "fileeeeeeeeeeeeeeeeeee",
@@ -20,6 +20,28 @@ Deno.test("format file names ", () => {
     "3                      8  ",
     "4                      9  ",
     "5                      10 ",
+  ];
+  assertEquals(FileFormat({ files: files, MaxRow: 5 }), Result);
+});
+Deno.test("format file names 2", () => {
+  const files = [
+    "hello1",
+    "fileeeeeeeeeeeeeeeeeee",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "helloworld",
+    "10",
+  ];
+  const Result = [
+    "hello1                 6                                ",
+    "fileeeeeeeeeeeeeeeeeee 7                                ",
+    "3                      8aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ",
+    "4                      helloworl                                ",
+    "5                      10                               ",
   ];
   assertEquals(FileFormat({ files: files, MaxRow: 5 }), Result);
 });
