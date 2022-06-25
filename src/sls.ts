6c15543cd6e3ee58a6c627df7412f61eb6b873af
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-26 07:28:30 +0900

    行数とか取得できるようになった

--- src/sls.ts
+++ src/sls.ts
@@ -2,11 +2,15 @@ import { SlAnimation } from "./lib/slAnimation.ts";
 import { ReadDirFile } from "./lib/ReadDir.ts";
 import { ChooesFileName } from "./lib/chooseFileName.ts";
 import { sleep } from "../deps.ts";
+import { getColumns, getLines } from "./lib/process.ts";
 async function sls(dir: string) {
   const fileList = await ReadDirFile(dir);
-
   let frame = 0;
   while (true) {
+    const Windowsize = {
+      collums: await getColumns(),
+      lines: await getLines(),
+    };
     const Sl = SlAnimation({
       files: ChooesFileName(fileList),
       slAnimationNumber: frame,
@@ -14,7 +18,14 @@ async function sls(dir: string) {
     console.clear();
 
     const SlAnimationText = Sl.join("\n");
-    console.log("frame", frame);
+    console.log(
+      "frame",
+      frame,
+      "collums",
+      Windowsize.collums,
+      "lines",
+      Windowsize.lines,
+    );
     console.log(SlAnimationText);
     await sleep(1 / 12);
     frame++;
