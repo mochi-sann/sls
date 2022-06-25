1142f6528b2ff67d322c9dd89cb7947a14256c6b
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-25 12:53:25 +0900

    小さい方はいい感じにできた

--- src/lib/createWagon.ts
+++ src/lib/createWagon.ts
@@ -1,13 +1,15 @@
 import { SplitArry } from "./spilitArry.ts";
+import { stringWidth } from "../../deps.ts";
+import { FileFormat } from "./fileFormat.ts";
 import { SlAciiArt } from "./slAa.ts";
 type CreateWagonPropsType = {
   files: string[];
 };
 export const CreateWagon = (props: CreateWagonPropsType): string[] => {
   const cargo = SlAciiArt.cargo;
-  const FileArrySplit = SplitArry({
-    arry: props.files,
-    dividedOfNumber: cargo.maxContentHeigt,
+  const files = FileFormat({
+    files: props.files,
+    MaxRow: cargo.maxContentHeigt,
   });
   const ReturnValue: string[] = [
     cargo.wogon[0],
@@ -17,17 +19,29 @@ export const CreateWagon = (props: CreateWagonPropsType): string[] => {
     cargo.LeftAndRigntWall,
     cargo.LeftAndRigntWall,
     cargo.LeftAndRigntWall,
+    cargo.wogon[7],
     cargo.wogon[8],
     cargo.wogon[9],
   ];
+  const WagonWidth: number = Math.max(
+    stringWidth(
+      cargo.wogon[0],
+    ),
+    stringWidth(ReturnValue[2] + "" + files[0] + "|"),
+  );
 
-  FileArrySplit.map((fileList, fileListKey) => {
-    const stringLength = Math.max(
-      ...fileList.map((e) => e.length),
-    );
-
-    fileList.map((file, fileKey) => {
-    });
+  const addSpavefileNames = files.map((value, key) => {
+    let Filenames = ReturnValue[key + 2] + " " +
+      value;
+    const FilenamesWidth: number = stringWidth(Filenames);
+    if (WagonWidth > FilenamesWidth + 2) {
+      return Filenames += " ".repeat(WagonWidth - FilenamesWidth - 1);
+    } else {
+      return Filenames;
+    }
+  });
+  addSpavefileNames.map((fileRow, key) => {
+    ReturnValue[key + 2] = fileRow + "|";
   });
   return ReturnValue;
 };
