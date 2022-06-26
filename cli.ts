3003f22c54a60c719c64843d0b6a9f141ce2fee4
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-26 16:04:04 +0900

    add speed option

--- cli.ts
+++ cli.ts
@@ -3,9 +3,13 @@ import { Command } from "./deps.ts";
 const { options, args } = await new Command()
   .name("sls")
   .arguments("[dir]")
+  .option("-s, --speed <speed>", "set Sl speed", {
+    default: 30,
+  })
   .description("show filelist with sl")
   .version("1.0.0")
   .parse(Deno.args);
 // console.log("options ", options, "args", args);
 const Dir: string = args[0] || ".";
-sls(Dir);
+console.log({ options });
+sls(Dir, options.speed);
