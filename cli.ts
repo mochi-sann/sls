36fb5268fe92c112f5b45c144d2bd2123a2568d0
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-26 16:11:55 +0900

    update version

--- cli.ts
+++ cli.ts
@@ -7,7 +7,7 @@ const { options, args } = await new Command()
     default: 30,
   })
   .description("show filelist with sl")
-  .version("1.0.0")
+  .version("1.1.1")
   .parse(Deno.args);
 // console.log("options ", options, "args", args);
 const Dir: string = args[0] || ".";
