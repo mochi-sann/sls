be5617b5fa2597c034bb84d9353b31e25287d044
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-06-30 18:55:00 +0900

    fixed the version of stringWidth to v5.1.2

--- deps.ts
+++ deps.ts
@@ -3,6 +3,6 @@ export {
   assertEquals,
 } from "https://deno.land/std@0.65.0/testing/asserts.ts";
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
-import stringWidth from "https://cdn.skypack.dev/string-width";
+import stringWidth from "https://cdn.skypack.dev/string-width@v5.1.2";
 export { stringWidth };
 export { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
