5fe850aeddf15a0763936ae560022921aea89486
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-26 12:59:59 +0900

    setup npm build script

--- deps.ts
+++ deps.ts
@@ -5,5 +5,5 @@ export {
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
 import stringWidth from "https://cdn.skypack.dev/string-width@v5.1.2";
 export { stringWidth };
-export { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 export { parseFlags } from "https://deno.land/x/cliffy@v0.24.2/flags/mod.ts";
---
+++ scripts/build_npm.ts
@@ -0,0 +1,35 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt@0.28.0/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "sls",
+    version: Deno.args[0],
+    description: "hoge",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/mochi-sann/sls.git",
+    },
+    bugs: {
+      url: "https://github.com/mochi-sann/sls/issues/new",
+    },
+  },
+  typeCheck: false,
+  test: true,
+  declaration: true,
+  scriptModule: false,
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
