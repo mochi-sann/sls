4137f840ff360cb74c753aa1b56488573eee06c7
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-08-26 16:20:47 +0900

    fix typo to ReturnValue

--- src/lib/slAnimation.ts
+++ src/lib/slAnimation.ts
@@ -6,7 +6,7 @@ type SlAnimationType = {
 };
 
 export const SlAnimation = (props: SlAnimationType): string[] => {
-  const ReaturnValue: string[] = [];
+  const ReturnValue: string[] = [];
   const CoalWagon: string[] = SlAciiArt.coalWagon;
   const Cargo = CreateWagon({ files: props.files });
 
@@ -15,22 +15,22 @@ export const SlAnimation = (props: SlAnimationType): string[] => {
 
   SlAciiArt.slTop.map((value, slTopkey) => {
     // console.log(value + CoalWagon[slTopkey]);
-    ReaturnValue[slTopkey] = value + CoalWagon[slTopkey];
+    ReturnValue[slTopkey] = value + CoalWagon[slTopkey];
   });
 
   // render  wheel
   SlAciiArt.wheel[WheelNumber].map(
     (value1, slWheelKey) => {
       // console.log(value1 + CoalWagon[slWheelKey + 7]);
-      ReaturnValue[slWheelKey + 7] = value1 + CoalWagon[slWheelKey + 7];
+      ReturnValue[slWheelKey + 7] = value1 + CoalWagon[slWheelKey + 7];
     },
   );
 
   // render cal wagon
   Cargo.map((value, key) => {
-    ReaturnValue[key] = ReaturnValue[key] + value;
+    ReturnValue[key] = ReturnValue[key] + value;
   });
 
   // console.log(await ReadDirFile());
-  return ReaturnValue;
+  return ReturnValue;
 };
