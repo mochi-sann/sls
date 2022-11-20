ef78b7b80d9fba99453e1c1ebccd1b59d253cffe
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-20 09:47:07 +0900

    依存をアップデート

--- deps.ts
+++ deps.ts
@@ -5,5 +5,5 @@ export {
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
 import stringWidth from "https://esm.sh/string-width@5.1.2";
 export { stringWidth };
-export { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
-export { parseFlags } from "https://deno.land/x/cliffy@v0.24.2/flags/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { parseFlags } from "https://deno.land/x/cliffy@v0.25.4/flags/mod.ts";
